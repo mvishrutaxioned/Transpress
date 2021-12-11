6ef5eb64ebf5b915dd6acb2e4a946439d438cde5
VishrutMargam <vishrutm@axioned.com>
2021-12-11 14:42:21 +0530

    added open video functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,5 +36,36 @@ $(document).ready(() => {
         window.scroll({top: 0, behavior: "smooth"});
     })
 
+    // close video function
+    function closeVid(e) {
+        e.preventDefault()
+        $('.video').addClass('none')
+        $('video').get(0).pause();
+        $('video').get(0).currentTime = 0;
+        $('html, body').removeClass('hidden')
+    }
+
+    // display video funcitionality
+    $('.about figure').click(e => {
+        e.preventDefault()
+
+        $('.video').removeClass('none');
+        $('html, body').addClass('hidden')
+    })
+
+    // close video on click
+    $('#closeVid').click(e => {
+        closeVid(e)
+    })
+
+    // close video on clicking outside
+    if($('.video').hasClass('none') == true) {
+        $('main').mouseup(e => {
+            var vid = $('video');
+            if(!vid.is(e.target) && vid.has(e.target).length === 0) {
+                closeVid(e)
+            }
+        })
+    }
 
 })
